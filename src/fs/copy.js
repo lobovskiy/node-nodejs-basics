cc1fd8e65ecccb26239cd077cce0d42c7819a7bd
Artem Lobovskii <lobovskiy@yandex.ru>
2024-01-28 21:15:18 +0300

    feat: implement fs/copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,33 @@
+import path from 'node:path';
+import fsPromises from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const SOURCE_DIR_NAME = 'files';
+const DEST_DIR_NAME = 'files_copy';
+const ERROR_MESSAGE = 'FS operation failed';
+
 const copy = async () => {
-    // Write your code here 
+  const sourceDirPath = path.resolve(__dirname, SOURCE_DIR_NAME);
+  const destDirPath = path.resolve(__dirname, DEST_DIR_NAME);
+  const handleCopyError = () => {
+    throw new Error(ERROR_MESSAGE);
+  };
+
+  const sourceDirEntries = await fsPromises.readdir(sourceDirPath, { withFileTypes: true })
+    .catch(handleCopyError);
+  const sourceFiles = sourceDirEntries.filter(entry => entry.isFile());
+
+  await fsPromises.mkdir(destDirPath).catch(handleCopyError);
+  for (const file of sourceFiles) {
+    const sourcePath = path.join(sourceDirPath, file.name);
+    const destPath = path.join(destDirPath, file.name);
+
+    await fsPromises.copyFile(sourcePath, destPath).catch(handleCopyError);
+  }
 };
 
 await copy();
