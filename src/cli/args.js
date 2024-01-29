165d4dc94f5f1c061e3145b09cbe629df0b8813c
Artem Lobovskii <lobovskiy@yandex.ru>
2024-01-29 11:24:48 +0300

    feat: implement cli/args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
+const ARG_PREFIX = '--';
+const ARG_LIST_SEPARATOR = ', ';
+
 const parseArgs = () => {
-    // Write your code here 
+  const [execPath, appPath, ...args] = process.argv;
+  const argNames = args
+    .filter((arg) => arg.startsWith(ARG_PREFIX))
+    .map((name) => name.slice(ARG_PREFIX.length));
+
+  console.log(argNames.map((name, index) =>
+    `${name} is ${args[index * 2 + 1]}`).join(ARG_LIST_SEPARATOR)
+  );
 };
 
 parseArgs();
