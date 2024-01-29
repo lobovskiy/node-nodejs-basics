baa37866e49861dd562292bfd7f56661f5e7bf16
Artem Lobovskii <lobovskiy@yandex.ru>
2024-01-29 22:24:53 +0300

    feat: implement streams/write task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,34 @@
+import path from 'node:path';
+import fs from 'node:fs';
+import { createInterface } from 'node:readline';
+import { EOL } from 'node:os';
+import { fileURLToPath } from 'node:url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const CONTENT_DIR_NAME = 'files';
+const FILE_NAME = 'fileToWrite.txt';
+const WRITING_PROMPT = `Type something below to write in the "fileToWrite.txt". Press Ctrl + C to exit.${EOL}`;
+
 const write = async () => {
-    // Write your code here 
+  const filePath = path.resolve(__dirname, CONTENT_DIR_NAME, FILE_NAME);
+  const writeStream = fs.createWriteStream(filePath, { encoding: 'utf8', flags: 'a' });
+  const ioInterface = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: WRITING_PROMPT,
+  });
+
+  ioInterface.on('line', (input) => {
+    writeStream.write(input);
+    writeStream.write(EOL);
+  })
+  ioInterface.on('close', () => {
+    writeStream.close();
+  })
+  ioInterface.prompt();
 };
 
 await write();
