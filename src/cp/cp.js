fee660a78f09ca5fe47cdcd8f9fc7b5986ebdb96
Artem Lobovskii <lobovskiy@yandex.ru>
2024-01-30 01:18:57 +0300

    feat: implement cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
+import path from 'path';
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const CONTENT_DIR_NAME = 'files';
+const FILE_NAME = 'script.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const filePath = path.resolve(__dirname, CONTENT_DIR_NAME, FILE_NAME);
+
+  spawn('node', [filePath, ...args], {
+    stdio: [process.stdin, process.stdout, process.stderr],
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess( [1, 'thisIsArg2', false]);
