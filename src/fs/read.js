0957ffd655d93060400fdd241068d54fdc156179
Artem Lobovskii <lobovskiy@yandex.ru>
2024-01-29 09:48:09 +0300

    feat: implement fs/read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import path from 'node:path';
+import fsPromises from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const CONTENT_DIR_NAME = 'files';
+const FILE_NAME = 'fileToRead.txt';
+const ERROR_MESSAGE = 'FS operation failed';
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.resolve(__dirname, CONTENT_DIR_NAME, FILE_NAME);
+  const handleError = () => {
+    throw new Error(ERROR_MESSAGE);
+  };
+
+  await fsPromises.access(filePath).catch(handleError);
+  const fileData = await fsPromises.readFile(filePath, { encoding: 'utf8' });
+  console.log(fileData);
 };
 
 await read();
