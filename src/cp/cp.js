8724e89dcc7777cf9674b6207c5f41c87af2228b
Artem Lobovskii <lobovskiy@yandex.ru>
2024-01-30 01:25:32 +0300

    refactor: correct constant names in cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -6,12 +6,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const CONTENT_DIR_NAME = 'files';
-const FILE_NAME = 'script.js';
+const SCRIPT_MODULE_NAME = 'script.js';
 
 const spawnChildProcess = async (args) => {
-  const filePath = path.resolve(__dirname, CONTENT_DIR_NAME, FILE_NAME);
+  const scriptModulePath = path.resolve(__dirname, CONTENT_DIR_NAME, SCRIPT_MODULE_NAME);
 
-  spawn('node', [filePath, ...args], {
+  spawn('node', [scriptModulePath, ...args], {
     stdio: [process.stdin, process.stdout, process.stderr],
   });
 };
