2af36662d1df6dfcdb3b0b41b7d7dd2d31d12921
Artem Lobovskii <lobovskiy@yandex.ru>
2024-01-29 17:41:44 +0300

    feat: implement streams/read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import path from 'node:path';
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const CONTENT_DIR_NAME = 'files';
+const FILE_NAME = 'fileToRead.txt';
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.resolve(__dirname, CONTENT_DIR_NAME, FILE_NAME);
+  const readStream = fs.createReadStream(filePath, { encoding: 'utf8' });
+
+  readStream.pipe(process.stdout);
 };
 
 await read();
